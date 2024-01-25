d4dac6f96a5debdc75aec46d7fe63c6b7183b956
AtharvaNair04 <atharvanair04@gmail.com>
2024-01-25 11:03:40 +0530

    image updated

--- reconnect/lib/src/constants/image_strings.dart
+++ reconnect/lib/src/constants/image_strings.dart
@@ -2,4 +2,5 @@ class ImgaeConstants{
   static const String welcomeImage = "assets/images/welcomeImage.jpg";
   static const String LogoText = "assets/images/logo_with_title.png";
   static const String Logo = "assets/images/logo.png";
+  static const String login_page = "assets/images/login_page.png";
 }

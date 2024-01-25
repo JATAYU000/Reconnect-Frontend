095911373bace9c62f8234212a3ce8cea604a504
AtharvaNair04 <atharvanair04@gmail.com>
2024-01-25 14:14:35 +0530

    Search

---
+++ reconnect/lib/src/widgets/search.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:reconnect/src/constants/colors.dart';
+final TextEditingController _searchController = TextEditingController();
+
+SizedBox Search(
+  final int scwidth,
+  final int scheight
+){
+  return SizedBox(
+    width: scwidth * 0.712,
+    height:scheight*0.067,
+    child: TextField(
+      controller: _searchController,
+      decoration: InputDecoration(
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide(color: Color(ColorConstants.primary),
+          width:2.0,
+          )
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide(color: Color(ColorConstants.primary),
+          width:2.0,
+          )
+        ),
+        hintText: "Search your institute",
+        suffixIcon:IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {
+            
+          },
+        )
+        
+      ),
+    ),
+  );
+}
